f5978e062a738c776380d1656ef75e7681fa71f1
WUT_DDK_SFB <295054118@qq.com>
2019-02-02 21:27:38 +0800

    ready to merge, apache licence

--- sirv-e-sdk/bsp/libwrap/sys/kill.c
+++
@@ -1,9 +0,0 @@
-/* See LICENSE of license details. */
-
-#include <errno.h>
-#include "stub.h"
-
-int __wrap_kill(int pid, int sig)
-{
-  return _stub(EINVAL);
-}
--- sirv-e-sdk/bsp/libwrap/sys/unlink.c
+++
@@ -1,9 +0,0 @@
-/* See LICENSE of license details. */
-
-#include <errno.h>
-#include "stub.h"
-
-int __wrap_unlink(const char* name)
-{
-  return _stub(ENOENT);
-}
