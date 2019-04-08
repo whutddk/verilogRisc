6d3130f23fe69bb24c170cf739263a9bc2716596
Ruige_Lee <295054118@qq.com>
2019-04-08 16:59:27 +0800

    拿掉了所有leftover

--- rtl/e203/core/e203_ifu_ift2icb.v
+++ rtl/e203/core/e203_ifu_ift2icb.v
@@ -3,7 +3,7 @@
 // Engineer: Ruige_Lee
 // Create Date: 2019-02-17 17:25:12
 // Last Modified by:   Ruige_Lee
-// Last Modified time: 2019-04-08 16:42:50
+// Last Modified time: 2019-04-08 16:59:16
 // Email: 295054118@whut.edu.cn
 // Design Name:   
 // Module Name: e203_ifu_ift2icb
@@ -130,152 +130,6 @@ input  ifu2itcm_holdup
 		.clk     (clk),
 		.rst_n   (rst_n)
 	);
-
-// ===========================================================================
-//////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////
-/////// The itfctrl scheme introduction
-//
-// The instruction fetch is very tricky due to two reasons and purposes:
-//   (1) We want to save area and dynamic power as much as possible
-//   (2) The 32bits-length instructon may be in unaligned address
-//
-// In order to acheive above-mentioned purposes we define the tricky
-//   fetch scheme detailed as below.
-//
-///////
-// Firstly, several phrases are introduced here:
-//   * Fetching target: the target address region including
-//         ITCM,
-//         System Memory Fetch Interface or ICache
-//            (Note: Sys Mem and I cache are Exclusive with each other)
-//   * Fetching target's Lane: The Lane here means the fetching 
-//       target can read out one lane of data at one time. 
-//       For example: 
-//        * ITCM is 64bits wide SRAM, then it can read out one 
-//          aligned 64bits one time (as a lane)
-//        * System Memory is 32bits wide bus, then it can read out one 
-//          aligned 32bits one time (as a lane)
-//        * ICache line is N-Bytes wide SRAM, then it can read out one 
-//          aligned N-Bytes one time (as a lane)
-//   * Lane holding-up: The read-out Lane could be holding up there
-//       For examaple:
-//        * ITCM is impelemented as SRAM, the output of SRAM (readout lane)
-//          will keep holding up and not change until next time the SRAM
-//          is accessed (CS asserted) by new transaction
-//        * ICache data ram is impelemented as SRAM, the output of
-//          SRAM (readout lane) will keep holding up and not change until
-//          next time the SRAM is accessed (CS asserted) by new transaction
-//        * The system memory bus is from outside core peripheral or memory
-//          we dont know if it will hold-up. Hence, we assume it is not
-//          hoding up
-//   * Crossing Lane: Since the 32bits-length instruction maybe unaligned with 
-//       word address boundry, then it could be in a cross-lane address
-//       For example: 
-//        * If it is crossing 64bits boundry, then it is crossing ITCM Lane
-//        * If it is crossing 32bits boundry, then it is crossing System Memory Lane
-//        * If it is crossing N-Bytes boundry, then it is crossing ICache Lane
-//   * IR register: The fetch instruction will be put into IR register which 
-//       is to be used by decoder to decoding it at EXU stage
-//       The Lower 16bits of IR will always be loaded with new coming
-//       instructions, but in order to save dynamic power, the higher 
-//       16bits IR will only be loaded when incoming instruction is
-//       32bits-length (checked by mini-decode module upfront IR 
-//       register)
-//       Note: The source of IR register Din depends on different
-//         situations described in detailed fetching sheme
-//   * Leftover buffer: The ifetch will always speculatively fetch a 32bits
-//       back since we dont know the instruction to be fetched is 32bits or
-//       16bits length (until after it read-back and decoded by mini-decoder).
-//       When the new fetch is crossing lane-boundry from current lane
-//       to next lane, and if the current lane read-out value is holding up.
-//       Then new 32bits instruction to be fetched can be concatated by 
-//       "current holding-up lane's upper 16bits" and "next lane's lower 16bits".
-//       To make it in one cycle, we push the "current holding-up lane's 
-//       upper 16bits" into leftover buffer (16bits) and only issue one ifetch
-//       request to memory system, and when it responded with rdata-back, 
-//       directly concatate the upper 16bits rdata-back with leftover buffer
-//       to become the full 32bits instruction.
-//
-// The new ifetch request could encounter several cases:
-//   * If the new ifetch address is in the same lane portion as last fetch
-//     address (current PC):
-//     ** If it is crossing the lane boundry, and the current lane rdout is 
-//        holding up, then
-//        ---- Push current lane rdout's upper 16bits into leftover buffer
-//        ---- Issue ICB cmd request with next lane address 
-//        ---- After the response rdata back:
-//            ---- Put the leftover buffer value into IR lower 16bits
-//            ---- Put rdata lower 16bits into IR upper 16bits if instr is 32bits-long
-//
-//** If it is crossing the lane boundry, but the current lane rdout is not 
-//holding up, then
-//---- First cycle Issue ICB cmd request with current lane address 
-//---- Put rdata upper 16bits into leftover buffer
-//---- Second cycle Issue ICB cmd request with next lane address 
-//---- Put the leftover buffer value into IR lower 16bits
-//---- Put rdata upper 16bits into IR upper 16bits if instr is 32bits-long
-//
-//     ** If it is not crossing the lane boundry, and the current lane rdout is 
-//        holding up, then
-//        ---- Not issue ICB cmd request, just directly use current holding rdata
-//            ---- Put aligned rdata into IR (upper 16bits 
-//                    only loaded when instr is 32bits-long)
-//
-//** If it is not crossing the lane boundry, but the current lane rdout is 
-//not holding up, then
-//---- Issue ICB cmd request with current lane address, just directly use
-//current holding rdata
-//---- Put aligned rdata into IR (upper 16bits 
-//only loaded when instr is 32bits-long)
-//   
-//
-//* If the new ifetch address is in the different lane portion as last fetch
-//address (current PC):
-//** If it is crossing the lane boundry, regardless the current lane rdout is 
-//holding up or not, then
-//---- First cycle Issue ICB cmd reqeust with current lane address 
-//---- Put rdata upper 16bits into leftover buffer
-//---- Second cycle Issue ICB cmd reqeust with next lane address 
-//---- Put the leftover buffer value into IR lower 16bits
-//---- Put rdata upper 16bits into IR upper 16bits if instr is 32bits-long
-//
-//** If it is not crossing the lane boundry, then
-//---- Issue ICB cmd request with current lane address, just directly use
-//current holding rdata
-//---- Put aligned rdata into IR (upper 16bits 
-//only loaded when instr is 32bits-long)
-//
-// ===========================================================================
-
-
-
-
-
-// The current accessing PC is crossing the lane boundry,删除C指令后不会发生了
-// wire ifu_req_lane_cross = (ifu_req_pc[1] == 1'b1) ;
-
-// The current accessing PC is begining of the lane boundry
-	// wire ifu_req_lane_begin = 1'b1;//永远为真
-	
-
-// The scheme to check if the current accessing PC is same as last accessed ICB address
-//   is as below:
-//     * We only treat this case as true when it is sequentially instruction-fetch
-//         reqeust, and it is crossing the boundry as unalgned (1st 16bits and 2nd 16bits
-//         is crossing the boundry)
-//         ** If the ifetch request is the begining of lane boundry, and sequential fetch,
-//            Then:
-//                 **** If the last time it was prefetched ahead, then this time is accessing
-//                        the same address as last time. Otherwise not.
-//         ** If the ifetch request is not the begining of lane boundry, and sequential fetch,
-//            Then:
-//                 **** It must be access the same address as last time.
-//     * Note: All other non-sequential cases (e.g., flush, branch or replay) are not
-//          treated as this case
-//  
-// wire req_lane_cross_r;
-// wire ifu_req_lane_same =  1'b0 ;
 	
 	// The current accessing PC is same as last accessed ICB address
 	wire ifu_req_lane_holdup = ( ifu2itcm_holdup & (~itcm_nohold) );
@@ -319,8 +173,8 @@ input  ifu2itcm_holdup
 // wire state_2nd_exit_ena;
 
 	// Define some common signals and reused later to save gatecounts
-	wire icb_sta_is_idle    = (icb_state_r == ICB_STATE_IDLE   );
-	wire icb_sta_is_1st     = (icb_state_r == ICB_STATE_1ST    );
+	wire icb_sta_is_idle    = (icb_state_r == ICB_STATE_IDLE);
+	wire icb_sta_is_1st     = (icb_state_r == ICB_STATE_1ST);
 // wire icb_sta_is_wait2nd = (icb_state_r == ICB_STATE_WAIT2ND);
 // wire icb_sta_is_2nd     = (icb_state_r == ICB_STATE_2ND    );
 
@@ -421,7 +275,7 @@ input  ifu2itcm_holdup
 
 // wire rsp_instr_sel_icb_rsp = 1'b1;
 
-	wire [16-1:0] ifu_icb_rsp_rdata_lsb16 = ({16{icb_cmd2itcm_r}} & ifu2itcm_icb_rsp_rdata[15:0]);
+	// wire [16-1:0] ifu_icb_rsp_rdata_lsb16 = ({16{icb_cmd2itcm_r}} & ifu2itcm_icb_rsp_rdata[15:0]);
 
 
 	// The fetched instruction from ICB rdata bus need to be aligned by PC LSB bits
